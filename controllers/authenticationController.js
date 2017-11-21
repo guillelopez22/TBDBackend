095d47a114c73f822785debf83b88df227779a8d
Miguel Angel Ardon Enriquez <miguel.ardon@unitec.edu>
2017-11-21 03:10:03 -0600

    areglaado

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -16,10 +16,8 @@ exports.login = {
     console.log(request.payload.password);
     var password = String(SHA3(request.payload.password));
     empleado.find({"identificacion": request.payload.identidad, "password": password}, function(err, empleado){
-      // console.log('identificacion: ', request.payload.identidad, 'empleado', empleado[0])
       if(!err){
         if(empleado.length > 0){
-          // request.cookieAuth.set(empleado.identificacion);
           console.log("identidad: "+ empleado[0].identificacion+ " scope: "+ empleado[0].scope);
           return reply({identidad: empleado[0].identificacion, scope: empleado[0].scope, success:true});
         }else{
@@ -32,21 +30,21 @@ exports.login = {
                 return reply({message: boom.unauthorized('Wrong email or password'), success:false});
               }
             }
-            console.log("nomon");
             return reply({message: boom.unauthorized('Wrong email or password'), success:false});
-          }
-          console.log("nomon2");
-          return reply({ message: boom.notAcceptable('Error Executing Query'), success: false});
+            return reply({ message: boom.notAcceptable('Error Executing Query'), success: false});
+          });
         }
-      });
-    };
-    exports.logout = {
-      // auth: {
-      //   mode:'required',
-      //   strategy:'session'
-      // },
-      handler: function(request, reply) {
-        request.cookieAuth.clear();
-        return reply('Logout Successful!');
       }
-    };
+    });
+  }
+};
+exports.logout = {
+  // auth: {
+  //   mode:'required',
+  //   strategy:'session'
+  // },
+  handler: function(request, reply) {
+    request.cookieAuth.clear();
+    return reply('Logout Successful!');
+  }
+};
